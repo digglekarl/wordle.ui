dc89696bc75a64612a632ff4823049358bd04fd0
karl diggle <digglek@gmail.com>
2022-01-19 15:37:55 +0000

    Test material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
+
 import React from 'react';
+import { Container, Typography, TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        WORDLE
       </header>
+      <Container>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" size='small' />
+      </Container>
+      
     </div>
   );
 }
 
 export default App;
+
+/*
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Proficient Software
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example with TypeScript
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+*/
